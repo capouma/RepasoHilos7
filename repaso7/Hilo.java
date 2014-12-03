9a5759566d477cb68980ed3f78e1263391367896
cpousamarcote <cpousamarcote@danielcastelao.org>
2014-12-03 20:39:22 +0100

    Creamos la clase Hilo que extendera de Thread.

---
+++ repaso7/Hilo.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package repaso7;
+
+/**
+ *
+ * @author findag
+ */
+public class Hilo extends Thread
+{
+    public Hilo(String nombre)
+    {
+        super(nombre);
+    }
+    
+}
