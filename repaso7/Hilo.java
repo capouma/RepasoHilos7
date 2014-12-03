7d9bfc064729d56b9ade1a94d06fe094460a5457
cpousamarcote <cpousamarcote@danielcastelao.org>
2014-12-03 20:43:58 +0100

    Sobreescribimos el metodo run, insertamos un if preguntando por el hilo 1 y si es el el que llega lo dormimos.

--- repaso7/Hilo.java
+++ repaso7/Hilo.java
@@ -5,6 +5,9 @@
  */
 package repaso7;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author findag
@@ -14,6 +17,23 @@ public class Hilo extends Thread
     public Hilo(String nombre)
     {
         super(nombre);
+    }
+    
+    public void run()
+    {
+        // Preguntamos si el hilo que llega es el hilo 1 si es asi lo dormimos.
+        if(getName().equalsIgnoreCase("Hilo 1"))
+        {
+            try
+            {
+                sleep(500);
+            }
+            catch (InterruptedException ex)
+            {
+                System.out.println("Error " + ex);
+            }
+        }
+        System.out.println("Hola soy el " + getName());
     }
     
 }
