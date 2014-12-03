c1913fad1792e72f92040a9d852972e1e7061c6f
cpousamarcote <cpousamarcote@danielcastelao.org>
2014-12-03 20:38:21 +0100

    Definimos nuestros hilos y los lanzamos

--- repaso7/Repaso7.java
+++ repaso7/Repaso7.java
@@ -16,7 +16,13 @@ public class Repaso7 {
      */
     public static void main(String[] args)
     {
+        // Definimos los 2 hilos.
+        Hilo hilo1 = new Hilo("Hilo 1");
+        Hilo hilo2 = new Hilo("Hilo 2");
         
+        // Lanzamos nuestros hilos.
+        hilo1.start();
+        hilo2.start();
     }
     
 }
